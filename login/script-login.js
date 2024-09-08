2fe20b98c5358d7f2ae9a1989a4c2c280fe0ab76
Guimts16 <mtsguilherme991@gmail.com>
2024-09-08 20:17:35 -0300

    atualização

--- login/script-login.js
+++
@@ -1,7 +0,0 @@
-function login(email, password) {
-    if (email === "admin@gmail.com" && password === "admin") {
-        alert("Login successful!");
-    } else {
-        alert("Login failed. Please check your email and password.");
-    }
-}
---
+++ script-login.js
@@ -0,0 +1,28 @@
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    if (email != "" && password != "") {
+        if (email === "mtsguilherme991@gmail.com" && password === "3141592") {
+            window.location.href = "menu/index.html";
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+    } else {
+        return;
+    }
+}
+
+function showPassword() {
+    const x = document.getElementById("password").type;
+    const s = document.getElementById("view").textContent;
+    if (s === "Mostrar") {
+        document.getElementById("view").textContent = "Ocultar";
+    } else {
+        document.getElementById("view").textContent = "Mostrar";
+    }
+    if (x === "password") {
+        document.getElementById('password').type = 'text';
+    } else {
+        document.getElementById('password').type = 'password';
+    }
+}
