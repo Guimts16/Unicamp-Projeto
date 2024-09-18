63a4e0b0ed29929964c29082a38ea27de1f0fdc0
Guilherme <134653860+Guimts16@users.noreply.github.com>
2024-09-18 08:40:06 -0300

    Add files via upload

---
+++ menu/perfil/profile.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="profile.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <script src="script.js"></script>
+    <title>Unicamp • Home</title>
+
+</head>
+
+<body>
+    <?php
+        <div class="sidebar">
+            <img class="inicialLogo" id="uni2" src="../assets/unicamp.png"/>
+            <a href="#">
+                Página inicial
+            </a>
+            <a href="#">
+                Sobre
+            </a>
+            <a href="#">
+                Turmas
+            </a>
+            <a href="#">
+                Mensagens
+            </a>
+            <br>
+            <br>
+            <a href="pags/profile.html">
+                Perfil
+            </a>
+            <br>
+            <br>
+
+        </div>
+        <div class="front">
+            
+
+        </div>
+    ?>
+</body>
+
+</html>
--- menu/script.js
+++ menu/script.js
@@ -7,10 +7,10 @@ function changeTheme() {
     const uni = document.getElementById("uni");
     const uni2 = document.getElementById("uni2");
     const uniStyles = document.querySelector(".logo");
-
-    if (cor ==  "black") {
-        document.body.style.backgroundColor = "white";
-        sidebar.style.backgroundColor = "white";
+    console.log(cor)
+    if (cor ==  "rgb(33, 33, 33)") {
+        document.body.style.backgroundColor = "#e8e8e8";
+        sidebar.style.backgroundColor = "#e8e8e8";
         for ( i of a ) {
   
             i.style.color = "black";
@@ -18,21 +18,20 @@ function changeTheme() {
         logo.src = "assets/moon.png";
         texts.style.color = "black";
         uni.src = "assets/unicamp.png";
-        
         uni2.src = "assets/unicamp.png";
-        uniStyles.style.width = "200px"
+        uniStyles.style.width = "200px  "
 
         } else { 
-        document.body.style.backgroundColor = "black";
-        sidebar.style.backgroundColor = "black";
+        document.body.style.backgroundColor = "#212121";
+        sidebar.style.backgroundColor = "#212121";
         for ( i of a ) {
             i.style.color = "white";
         }
         logo.src = "assets/sun.png";
         texts.style.color = "white";
-        uni.src = "assets/unicamp-branco.png";
-        uni2.src = "assets/unicamp-branco.png";
-        uniStyles.style.width = "230px"
+        uni.src = "assets/uni-branco.png";
+        uni2.src = "assets/uni-branco.png";
+        uniStyles.style.width = "215px"
     }
 
 }
